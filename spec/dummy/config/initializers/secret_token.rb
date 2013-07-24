094fc414b0450f34b5cdd46ec07200624267a8fc
Jan Graichen <jg@altimos.de>
2013-07-24 14:55:02 +0200

    Add default rails dummy app.

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '20d073800b0472235d80d42b7949d08374d9ac4e7562947d610d082ad1510d72b7d302a76b4f8122a2b05831794ef5b491e8f9ffadc484133b4fd1b74d2ab736'
