0578f63c3893597c4a04cb6c3ce60431698e71c7
Jan Graichen <jg@altimos.de>
2013-08-19 16:47:17 +0200

    Check for pending migration only on Rails 4.

--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -21,7 +21,7 @@ Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration) && Rails::VERSION::MAJOR >= 4
 
 RSpec.configure do |config|
 
