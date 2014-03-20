289e237b755b1110e0a1c748845b311cc8f3ae36
Jan Graichen <jg@altimos.de>
2014-03-20 16:02:48 +0100

    Release 0.5.0

--- lib/msgr/version.rb
+++ lib/msgr/version.rb
@@ -1,8 +1,8 @@
 module Msgr
   module VERSION
     MAJOR = 0
-    MINOR = 4
-    PATCH = 1
+    MINOR = 5
+    PATCH = 0
     STAGE = nil
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.')
 
