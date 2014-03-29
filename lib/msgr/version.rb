22a00db3b40624c2ac9635cdfd5d31a716c1967c
Jan Graichen <jg@altimos.de>
2014-03-29 13:41:41 +0100

    Release 0.11.0.rc1 with configurable pool class.

--- lib/msgr/version.rb
+++ lib/msgr/version.rb
@@ -1,9 +1,9 @@
 module Msgr
   module VERSION
     MAJOR = 0
-    MINOR = 10
-    PATCH = 2
-    STAGE = nil
+    MINOR = 11
+    PATCH = 0
+    STAGE = 'rc1'
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.')
 
     def self.to_s; STRING end
