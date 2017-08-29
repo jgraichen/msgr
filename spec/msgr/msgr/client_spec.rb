jgraichen/msgr

commit 519de9c31966a3ec78bc9a5a82da7718fa63442d

Allow passing AMQP connection parameters as hash
This commit changes the configuration parsing code to correctly accept
AMQP connection parameters as a hash and add support for slash-less
vhost arguments. Previously an error was raised when trying to assign a
slash-less vhost as a URI path. The actual `uri` configuration is still
parsed in the previous non-AMQP variant using the actual URI path.

This commit allows a configuration as the following:

    production:
      uri: amqp://user@host/vhost
      pass: secret
      vhost: real_vhost
      port: 1234

With the above configuration MSGR will try to connect to a broker at
`host:1234`, authenticate with `user` and `secret` and using the
vhost `real_vhost`.

- Fix #13

lib/msgr/client.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
+
+require 'uri'
+require 'cgi'
+
 module Msgr
   # rubocop:disable Metrics/ClassLength
   class Client
     include Logging
-    attr_reader :uri, :config
 
-    # rubocop:disable Metrics/AbcSize
-    # rubocop:disable Metrics/MethodLength
+    attr_reader :config
+
+    # rubocop:disable MethodLength
     def initialize(config = {})
-      @uri = ::URI.parse config[:uri] ? config.delete(:uri) : 'amqp://localhost/'
-      config[:pass] ||= @uri.password
+      @config = {
+        host: '127.0.0.1',
+        vhost: '/',
+        max: 2
+      }
 
-      @uri.user   = config[:user] ||= @uri.user || 'guest'
-      @uri.scheme = (config[:ssl] ||= @uri.scheme.to_s.casecmp('amqps').zero?) ? 'amqps' : 'amqp'
-      @uri.host   = config[:host] ||= @uri.host || '127.0.0.1'
-      @uri.port   = config[:port] ||= @uri.port
-      @uri.path   = config[:vhost] ||= @uri.path.present? ? @uri.path : '/'
-      config.reject! {|_, v| v.nil? }
-
-      @config = config
-      @config[:max] ||= 2
+      @config.merge! parse(config.delete(:uri)) if config.key?(:uri)
+      @config.merge! config.symbolize_keys
 
       @mutex  = ::Mutex.new
       @routes = Routes.new
@@ -27,6 +27,30 @@ module Msgr
 
       log(:debug) { "Created new client on process ##{@pid}..." }
     end
+    # rubocop:enable all
+
+    # rubocop:disable AbcSize
+    # rubocop:disable MethodLength
+    # rubocop:disable PerceivedComplexity
+    # rubocop:disable CyclomaticComplexity
+    def uri
+      @uri = begin
+        uri = ::URI.parse('amqp://localhost')
+
+        uri.user     = CGI.escape(config[:user]) if config.key?(:user)
+        uri.password = '****'                    if config.key?(:pass)
+        uri.host     = config[:host]             if config.key?(:host)
+        uri.port     = config[:port]             if config.key?(:port)
+        uri.scheme   = config[:ssl] ? 'amqps' : 'amqp'
+
+        if config.key?(:vhost) && config[:vhost] != '/'
+          uri.path = "/#{CGI.escape(config[:vhost])}"
+        end
+
+        uri
+      end
+    end
+    # rubocop:enable all
 
     def running?
       mutex.synchronize do
@@ -35,6 +59,7 @@ module Msgr
       end
     end
 
+    # rubocop:disable AbcSize
     def start
       mutex.synchronize do
         check_process!
@@ -47,6 +72,7 @@ module Msgr
         connection.bind(@routes)
       end
     end
+    # rubocop:enable all
 
     def connect
       mutex.synchronize do
@@ -59,6 +85,7 @@ module Msgr
       end
     end
 
+    # rubocop:disable AbcSize
     def stop(opts = {})
       mutex.synchronize do
         check_process!
@@ -73,6 +100,7 @@ module Msgr
         reset
       end
     end
+    # rubocop:enable all
 
     def purge(release: false)
       mutex.synchronize do
@@ -155,5 +183,23 @@ module Msgr
       @bindings   = nil
       @dispatcher = nil
     end
+
+    # rubocop:disable AbcSize
+    def parse(uri)
+      # Legacy parsing of URI configuration; does not follow usual
+      # AMQP vhost encoding but used regular URL path
+      uri = ::URI.parse(uri)
+
+      config = {}
+      config[:user]  ||= uri.user     if uri.user
+      config[:pass]  ||= uri.password if uri.password
+      config[:host]  ||= uri.host     if uri.host
+      config[:port]  ||= uri.port     if uri.port
+      config[:vhost] ||= uri.path     if uri.path
+      config[:ssl]   ||= uri.scheme.casecmp('amqps').zero?
+
+      config
+    end
+    # rubocop:enable all
   end
 end

commit 1abfb9b66c68b96a0c55a55313899dcab066fb31

Fix URI parsing with empty vhost
This was broken in 519de9c. Refs #13.

Also, write some tests for config parsing to
prevent future regressions.

lib/msgr/client.rb
@@ -195,7 +195,7 @@ module Msgr
       config[:pass]  ||= uri.password if uri.password
       config[:host]  ||= uri.host     if uri.host
       config[:port]  ||= uri.port     if uri.port
-      config[:vhost] ||= uri.path     if uri.path
+      config[:vhost] ||= uri.path     unless uri.path.empty?
       config[:ssl]   ||= uri.scheme.casecmp('amqps').zero?
 
       config

spec/msgr/msgr/client_spec.rb
@@ -1,0 +1,59 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe Msgr::Client do
+  subject { described_class.new config }
+  let(:config) { {} }
+
+  describe '#uri' do
+    subject { super().uri.to_s }
+
+    context 'with default config' do
+      it 'uses the default config' do
+        is_expected.to eq 'amqp://127.0.0.1'
+      end
+    end
+
+    context 'with overwritten URI' do
+      context 'without vhost' do
+        let(:config) { {uri: 'amqp://rabbit'} }
+
+        it 'does not specify a vhost' do
+          is_expected.to eq 'amqp://rabbit'
+        end
+      end
+
+      context 'with empty vhost' do
+        let(:config) { {uri: 'amqp://rabbit/'} }
+
+        it 'does not specify a vhost' do
+          is_expected.to eq 'amqp://rabbit'
+        end
+      end
+
+      context 'with explicit vhost' do
+        let(:config) { {uri: 'amqp://rabbit/some_vhost'} }
+
+        # This behavior is due to legacy parsing in Msgr's config.
+        # We interpret the entire path (incl. the leading slash)
+        # as vhost. As per AMQP rules, this means the leading slash
+        # is part of the vhost, which means it has to be URL encoded.
+        # This will likely change with the next major release.
+        it 'uses the entire path as vhost' do
+          is_expected.to eq 'amqp://rabbit/%2Fsome_vhost'
+        end
+      end
+    end
+
+    context 'with URI and vhost' do
+      let(:config) { {uri: 'amqp://rabbit/some_vhost', vhost: 'real_vhost'} }
+
+      # This is currently the only way to specify a vhost without
+      # leading slash (as a vhost in the :uri config would have
+      # an extra URL encoded leading slash).
+      it 'uses the explicit vhost' do
+        is_expected.to eq 'amqp://rabbit/real_vhost'
+      end
+    end
+  end
+end