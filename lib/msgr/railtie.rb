a37ea1bc6a4f300fbdd68362a473303bab6706cd
Jan Graichen <jgraichen@altimos.de>
2020-12-16 16:47:21 +0100

    Static default options set wrong config path
    Static setting DEFAULT_OPTIONS failed with setting "/config/msgr.rb" as
    routing file some times.

--- lib/msgr/railtie.rb
+++ lib/msgr/railtie.rb
@@ -4,15 +4,6 @@ module Msgr
   class Railtie < ::Rails::Railtie
     config.msgr = ActiveSupport::OrderedOptions.new
 
-    DEFAULT_OPTIONS = {
-      checkcredentials: true,
-      routing_file: "#{Rails.root}/config/msgr.rb"
-    }.freeze
-
-    if File.exist?("#{Rails.root}/app/consumers")
-      config.autoload_paths << File.expand_path("#{Rails.root}/app/consumers")
-    end
-
     initializer 'msgr.logger' do |app|
       app.config.msgr.logger ||= Rails.logger
     end
@@ -20,8 +11,7 @@ module Msgr
     initializer 'msgr.start' do
       config.after_initialize do |app|
         Msgr.logger = app.config.msgr.logger
-
-        self.class.load(app.config_for(:rabbitmq))
+        self.class.load(app.config_for(:rabbitmq).symbolize_keys)
       end
     end
 
@@ -31,7 +21,11 @@ module Msgr
 
     class << self
       def load(config)
-        config = DEFAULT_OPTIONS.merge(config)
+        # Set defaults
+        config.reverse_merge!(
+          checkcredentials: true,
+          routing_file: Rails.root.join('config/msgr.rb').to_s
+        )
 
         Msgr.config = config
         Msgr.client.connect if config.fetch(:checkcredentials)
